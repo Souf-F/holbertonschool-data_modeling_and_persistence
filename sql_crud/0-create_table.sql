fc56b9f54921cd80d91409ade0036d40591a897d
Souf-F <sfecom.31000@gmail.com>
2026-06-01 10:05:37 +0200

    make genre, price, stock, and published_year fields NOT NULL in books table

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE books (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     author TEXT NOT NULL,
-    genre TEXT,
-    price REAL,
-    stock INTEGER,
-    published_year INTEGER
+    genre TEXT NOT NULL,
+    price REAL NOT NULL,
+    stock INTEGER NOT NULL,
+    published_year INTEGER NOT NULL
 );
