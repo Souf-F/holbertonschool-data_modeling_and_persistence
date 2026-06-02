6cd02bc5b7de8e5ebbb44e7dd8000dab69bdf25c
Souf-F <sfecom.31000@gmail.com>
2026-06-02 11:00:19 +0200

    fix: add missing newline at end of SQL script for instructors with active courses

--- sql_join/15-instructors-with-active-courses.sql
+++ sql_join/15-instructors-with-active-courses.sql
@@ -2,4 +2,4 @@ SELECT DISTINCT instructors.name AS instructor_name
 FROM instructors
 INNER JOIN courses ON instructors.id = courses.instructor_id
 INNER JOIN registrations ON courses.id = registrations.course_id
-ORDER BY instructor_name;
+ORDER BY instructor_name;
